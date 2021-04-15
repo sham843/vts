68e972a14751e54a2681c875c116129ec035ef30
Sham843 <you@example.com>
2021-04-15 08:23:46 +0530

    commit

--- src/app/partial/layout/header/header.component.ts
+++ src/app/partial/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-header',
@@ -6,15 +8,24 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  getMobileNo: any;
+  getUserName: any
   @Output() onToggleSidebar: EventEmitter<any> = new EventEmitter();
-  constructor() { }
+  constructor(private router: Router, private _commonService: CommonService) { }
 
   ngOnInit(): void {
+    this.getMobileNo = this._commonService.loggedInUserMobile();
+    this.getUserName = this._commonService.loggedInUsername();
   }
 
   toggleSidebar() {
     this.onToggleSidebar.emit();
+  }
+
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['../login']);
   }
 
 }
