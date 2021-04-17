5075d2a67c5da70933a0776dec353423f7be8155
avinash_rathod <avinash.rathod@shauryatechnosoft.com>
2021-04-17 11:33:17 +0530

    commit

--- src/app/partial/dashboard/dashboard.component.ts
+++ src/app/partial/dashboard/dashboard.component.ts
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import { Component, OnInit,ViewChild} from '@angular/core';
-import { CommonService } from 'src/app/services/common.service';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { Label, MultiDataSet } from 'ng2-charts';
-import { ChartType } from 'chart.js';
-import { ToastrService } from 'ngx-toastr';
-import { SingleDataSet, PluginServiceGlobalRegistrationAndOptions } from 'ng2-charts';
-import { MatSort } from '@angular/material/sort';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-
-=======
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -23,7 +10,6 @@ import { EtpDetailsDialogComponent } from './dialog/etp-details-dialog/etp-detai
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
->>>>>>> master
 
 @Component({
   selector: 'app-dashboard',
@@ -46,37 +32,12 @@ export class DashboardComponent implements OnInit {
   totalVehicles: any;
   loggedInUserId = this._commonService.loggedInUserId();
 
-<<<<<<< HEAD
-  userRole: any;
-  resBreakdownReport: any;
-  resInvoiceDetails: any;
-  resSimDueAndOverDue: any;
-  resComplaintDetails: any;
-  resVehicleStatusCount: any;
-  resVehiclesCountDashboard: any;
-  piChartLabel: any = [];
-  piChartValue: any = [];
-  loggedInUserId = this._commonService.loggedInUserId();
-  dataSource: any;
-
-  displayedColumns: string[] = ['srNo','vehicleNo','invoiceNo','quantity','destination',
-                              'validityFrom','validityUpto','breakdownDatetime','restartDatetime'];
-
-   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-
-  constructor(private _commonService: CommonService,
-    private spinner: NgxSpinnerService,
-    private toastrService: ToastrService
-  ) { }
-=======
   constructor(public dialog: MatDialog,
     private _commonService: CommonService,
     private _callAPIService:CallAPIService,
     private spinner: NgxSpinnerService,
     private router: Router,
     private _snackBar: MatSnackBar) { }
->>>>>>> master
 
   ngOnInit(): void {
     this.userRole = this._commonService.userRole();
@@ -86,25 +47,6 @@ export class DashboardComponent implements OnInit {
     this.getComplaintDetails();
     this.getVehicleStatusCount();
     this.getVehiclesCountDashboard();
-<<<<<<< HEAD
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  getBreakdownReport() {
-    this.spinner.show();
-    this._commonService.setHttp('get', 'vehicle-tracking/dashboard/get-breakdown-report?UserId=' + this.loggedInUserId, false, false, false, 'vehicleTrackingBaseUrlApi');
-    this._commonService.getHttp().subscribe((res: any) => {
-      if (res.statusCode === "200") {
-        this.resBreakdownReport = res;
-        this.resBreakdownReport = this.resBreakdownReport?.responseData
-        this.dataSource = new MatTableDataSource(this.resBreakdownReport);
-         this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-=======
 
   }
 
@@ -116,18 +58,10 @@ export class DashboardComponent implements OnInit {
       if (res.statusCode === "200") {
         this.resBreakdownReport = res;
         this.resBreakdownReport = this.resBreakdownReport?.responseData
->>>>>>> master
         this.spinner.hide();
       }
       else if (res.statusCode === "409") {
         this.spinner.hide();
-<<<<<<< HEAD
-        this.toastrService.error(res.statusMessage);
-      }
-      else {
-        this.spinner.hide();
-        // this.toastrService.error(res.statusMessage);
-=======
         this._snackBar.open(res.statusMessage, "Close", {
           duration: 1000,
         });
@@ -137,20 +71,14 @@ export class DashboardComponent implements OnInit {
         this._snackBar.open(res.statusMessage, "Close", {
           duration: 1000,
         });
->>>>>>> master
       }
     })
   }
 
   getInvoiceDetails() {
     this.spinner.show();
-<<<<<<< HEAD
-    this._commonService.setHttp('get', 'vehicle-tracking/dashboard/get-invoice-details?UserId=' + this.loggedInUserId, false, false, false, 'vehicleTrackingBaseUrlApi');
-    this._commonService.getHttp().subscribe((res: any) => {
-=======
     this._callAPIService.callAPI('get', 'vehicle-tracking/dashboard/get-invoice-details?UserId=' + this.loggedInUserId, false, false, false, 'vehicleTrackingBaseUrlApi');
     this._callAPIService.getResponse().subscribe((res: any) => {
->>>>>>> master
       this.resInvoiceDetails = res;
       this.resInvoiceDetails = this.resInvoiceDetails?.responseData;
       if (res.statusCode === "200") {
@@ -158,13 +86,9 @@ export class DashboardComponent implements OnInit {
       }
       else if (res.statusCode === "409") {
         this.spinner.hide();
-<<<<<<< HEAD
-        this.toastrService.error(res.statusMessage);
-=======
         this._snackBar.open(res.statusMessage, "Close", {
           duration: 1000,
         });
->>>>>>> master
       }
       else {
         this.spinner.hide();
@@ -175,26 +99,17 @@ export class DashboardComponent implements OnInit {
 
   getSimDueAndOverDue() {
     this.spinner.show();
-<<<<<<< HEAD
-    this._commonService.setHttp('get', 'vehicle-tracking/dashboard/get-sim-due-and-over-due?UserId=' + this.loggedInUserId, false, false, false, 'vehicleTrackingBaseUrlApi');
-    this._commonService.getHttp().subscribe((res: any) => {
-=======
     this._callAPIService.callAPI('get', 'vehicle-tracking/dashboard/get-sim-due-and-over-due?UserId=' + this.loggedInUserId, false, false, false, 'vehicleTrackingBaseUrlApi');
     this._callAPIService.getResponse().subscribe((res: any) => {
->>>>>>> master
       if (res.statusCode === "200") {
         this.resSimDueAndOverDue = res;
         this.spinner.hide();
       }
       else if (res.statusCode === "409") {
         this.spinner.hide();
-<<<<<<< HEAD
-        this.toastrService.error(res.statusMessage);
-=======
         this._snackBar.open(res.statusMessage, "Close", {
           duration: 1000,
         });
->>>>>>> master
       }
       else {
         this.spinner.hide();
@@ -205,26 +120,14 @@ export class DashboardComponent implements OnInit {
 
   getComplaintDetails() {
     this.spinner.show();
-<<<<<<< HEAD
-    this._commonService.setHttp('get', 'vehicle-tracking/dashboard/get-complaint-details?UserId=' + this.loggedInUserId, false, false, false, 'vehicleTrackingBaseUrlApi');
-    this._commonService.getHttp().subscribe((res: any) => {
-=======
     this._callAPIService.callAPI('get', 'vehicle-tracking/dashboard/get-complaint-details?UserId=' + this.loggedInUserId, false, false, false, 'vehicleTrackingBaseUrlApi');
     this._callAPIService.getResponse().subscribe((res: any) => {
->>>>>>> master
       if (res.statusCode === "200") {
         this.resComplaintDetails = res;
         this.spinner.hide();
       }
       else if (res.statusCode === "409") {
         this.spinner.hide();
-<<<<<<< HEAD
-        this.toastrService.error(res.statusMessage);
-      }
-      else {
-        this.spinner.hide();
-        //  this.toastrService.error(res.statusMessage);
-=======
         this._snackBar.open(res.statusMessage, "Close", {
           duration: 1000,
         });
@@ -234,114 +137,11 @@ export class DashboardComponent implements OnInit {
         this._snackBar.open(res.statusMessage, "Close", {
           duration: 1000,
         });
->>>>>>> master
       }
 
     })
   }
 
-<<<<<<< HEAD
-  // for PiChart
-  options: any = { legend: { position: 'right', } }
-  doughnutChartLabels: Label[] = this.piChartLabel; // pichart key
-  doughnutChartData: MultiDataSet = [this.piChartValue];// pichart value
-  doughnutChartType: ChartType = 'doughnut';
-
-  public doughnutChartPlugins: PluginServiceGlobalRegistrationAndOptions[] = [{
-    afterDraw(chart:any) {
-      const ctx = chart.ctx;
-      var txt1 = '';
-      var txt2 = '';    
-
-      try{
-        var check = chart.active ? chart.tooltip._active[0]._datasetIndex : "None";
-        if(check !== "None"){
-        txt1 = chart.tooltip._data.labels[chart.tooltip._active[0]._index];
-        txt2 = chart.tooltip._data.datasets[0].data[chart.tooltip._active[0]._index];        
-      }else{
-        txt1 = chart.tooltip._data.labels[0];
-        txt2 = chart.tooltip._data.datasets[0].data[0];
-      }
-      }
-      catch(err){
-        txt1 = chart.tooltip._data.labels[0] 
-        txt2 = chart.tooltip._data.datasets[0].data[0];
-      }
-      //Get options from the center object in options
-      const sidePadding = 60;
-      const sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2)
-
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      const centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
-      const centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
-
-      //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
-
-      const stringWidth = ctx.measureText(txt1).width;
-      const elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
-
-      // Find out how much the font can grow in width.
-      const widthRatio = elementWidth / stringWidth;
-      const newFontSize = Math.floor(30 * widthRatio);
-      const elementHeight = (chart.innerRadius * 2);
-
-      // Pick a new font size so it will not be larger than the height of label.
-      const fontSizeToUse = 30;
-      ctx.font = fontSizeToUse + 'px Arial';
-      ctx.fillStyle = 'black';
-
-      // Draw text in center
-      ctx.fillText(10, centerX, centerY - 10);
-      var fontSizeToUse1 = 15;
-      ctx.font = fontSizeToUse1 + 'px Arial';
-      ctx.fillText(txt1, centerX, centerY + 10);
-    }
-  }];
-
-
-  getVehicleStatusCount() {
-    this.spinner.show();
-    this._commonService.setHttp('get', 'vehicle-tracking/tracking/get-vehicle-status-count?UserId=' + this.loggedInUserId, false, false, false, 'vehicleTrackingBaseUrlApi');
-    this._commonService.getHttp().subscribe((res: any) => {
-      if (res.statusCode === "200") {
-        this.spinner.hide();
-        this.resVehicleStatusCount = res;
-        for (var key in this.resVehicleStatusCount.responseData) {
-          if (this.resVehicleStatusCount.responseData[key] > 1) {
-            let keys;
-            if (key == "totalVehicles") {
-              keys = "Total Vehicles" + " (" + this.resVehicleStatusCount.responseData.totalVehicles + ")";
-            } else if (key == "idleVehicles") {
-              keys = "Idle Vehicles" + " (" + this.resVehicleStatusCount.responseData.idleVehicles + ")";
-            } else if (key == "isGpsService") {
-              keys = "Gps Service" + " (" + this.resVehicleStatusCount.responseData.isGpsService + ")";
-            } else if (key == "noPollingVehicles") {
-              keys = "No Polling Vehicles" + " (" + this.resVehicleStatusCount.responseData.noPollingVehicles + ")";
-            } else if (key == "offlineVehicles") {
-              keys = "Offline Vehicles" + " (" + this.resVehicleStatusCount.responseData.offlineVehicles + ")";
-            } else if (key == "runningVehicles") {
-              keys = "Running Vehicles" + " (" + this.resVehicleStatusCount.responseData.runningVehicles + ")";
-            } else if (key == "stopVehicles") {
-              keys = "Stop Vehicles" + " (" + this.resVehicleStatusCount.responseData.stopVehicles + ")";
-            }
-            else{
-              console.log("no data");
-            }
-            this.piChartLabel.push(keys);
-            this.piChartValue.push(this.resVehicleStatusCount.responseData[key]);
- 
-          }
-        }
-      }
-      else if (res.statusCode === "409") {
-        this.spinner.hide();
-        this.toastrService.error(res.statusMessage);
-      }
-      else {
-        this.spinner.hide();
-        this.toastrService.error(res.statusMessage);
-=======
   getVehicleStatusCount() {
     this.spinner.show();
     this._callAPIService.callAPI('get', 'vehicle-tracking/tracking/get-vehicle-status-count?UserId=' + this.loggedInUserId, false, false, false, 'vehicleTrackingBaseUrlApi');
@@ -372,19 +172,12 @@ export class DashboardComponent implements OnInit {
         this._snackBar.open(res.statusMessage, "Close", {
           duration: 1000,
         });
->>>>>>> master
       }
 
     })
   }
 
 
-<<<<<<< HEAD
-  getVehiclesCountDashboard() {
-    this.spinner.show();
-    this._commonService.setHttp('get', 'vehicle-tracking/dashboard/get-vehicles-count-dashboard?UserId=' + this.loggedInUserId, false, false, false, 'vehicleTrackingBaseUrlApi');
-    this._commonService.getHttp().subscribe((res: any) => {
-=======
   pieChart(stopVehicles: any, offlineVehicles: any, runningVehicles: any, totalVehicles: any) {
 
     am4core.useTheme(am4themes_animated);
@@ -473,23 +266,12 @@ export class DashboardComponent implements OnInit {
     this.spinner.show();
     this._callAPIService.callAPI('get', 'vehicle-tracking/dashboard/get-vehicles-count-dashboard?UserId=' + this.loggedInUserId, false, false, false, 'vehicleTrackingBaseUrlApi');
     this._callAPIService.getResponse().subscribe((res: any) => {
->>>>>>> master
       if (res.statusCode === "200") {
         this.resVehiclesCountDashboard = res;
         this.spinner.hide();
       }
       else if (res.statusCode === "409") {
         this.spinner.hide();
-<<<<<<< HEAD
-        this.toastrService.error(res.statusMessage);
-      }
-      else {
-        this.spinner.hide();
-        //this.toastrService.error(res.statusMessage);
-      }
-
-    })
-=======
         this._snackBar.open(res.statusMessage, "Close", {
           duration: 1000,
         });
@@ -516,7 +298,6 @@ export class DashboardComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
     });
->>>>>>> master
   }
 
   openDialogeTPDetails(): void {
